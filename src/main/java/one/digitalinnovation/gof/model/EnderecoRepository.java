1f7f6fe8267b66b5dbd501ed74c6fe731911bac6
Diego Rapichan <direrapichan@gmail.com>
2026-01-20 11:47:27 -0300

    Add comment to EnderecoRepository interface
    Added a comment to indicate the purpose of the interface.

--- src/main/java/one/digitalinnovation/gof/model/EnderecoRepository.java
+++ src/main/java/one/digitalinnovation/gof/model/EnderecoRepository.java
@@ -2,7 +2,7 @@ package one.digitalinnovation.gof.model;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+// Interface
 @Repository
 public interface EnderecoRepository extends CrudRepository<Endereco, String> {
 
